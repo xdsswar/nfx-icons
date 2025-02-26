f5cbd4e794b1e9a20d4e8313f5b45ada88c58af1
XDSSWAR <xdsswar@gmail.com>
2025-02-26 11:41:15 -0500

    Removed cp headers

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,14 +1,4 @@
-/*
- * Copyright © 2025. XTREME SOFTWARE SOLUTIONS
- *
- * All rights reserved. Unauthorized use, reproduction, or distribution
- * of this software or any portion of it is strictly prohibited and may
- * result in severe civil and criminal penalties. This code is the sole
- * proprietary of XTREME SOFTWARE SOLUTIONS.
- *
- * Commercialization, redistribution, and use without explicit permission
- * from XTREME SOFTWARE SOLUTIONS, are expressly forbidden.
- */
+
 
 /**
  * @author XDSSWAR
